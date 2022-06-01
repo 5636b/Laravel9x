eefb6ca27ca840c751b8e72713471064feb88ed7
5636b <mush.comushroomhead@hotmail.com>
2022-05-31 23:56:50 -0300

    Aprendendo novas formas de fazer rotas

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,30 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::prefix('usuarios')->group(function () {
+    Route::get('', function () {
+        return 'usuário';
+    })->name('usuarios');
+
+    Route::get('/{id}', function () {
+        return 'Mostrar infos';
+    })->name('usuarios.show');
+    
+    Route::get('/{id}/tags', function () {
+        return 'Tags 🧭 ';
+    })->name('usuarios.tags');
+}); // /usuarios/edit -> Assim ficaria o domínio criado.
+
+Route::get('/empresa/{string?}', function ($string = null) {
+    return $string;
+});
+
+Route::get('/users/{id?}', function ($id = null) {
+    return $id;
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
